68a375b567fd81f168f6d5cc6e0b0cd30ade7d0f
Yurina Shin <yurinas@gmail.com>
2018-08-30 17:15:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+  get '/' do
+    erb :team
+  end
+   post'/teams' do
+     @team = Team.new
+     @team = params["team"]["name"]
+     @motto = params[:team][:motto]
+     @hero = Hero.new
 
+     erb :super_hero
+   end
 
 end
---
+++ app/models/team.rb
@@ -0,0 +1,16 @@
+class Team
+  attr_accessor :name, :motto
+  @@all = []
+  def initiazie(params)
+    @name = params[:name]
+    @motto = params[:motto]
+    
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
+end
