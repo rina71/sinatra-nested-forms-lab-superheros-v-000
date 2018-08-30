6b1464880ee21fff7778014b1404b397bf0758e0
Yurina Shin <yurinas@gmail.com>
2018-08-30 17:39:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class App < Sinatra::Base
   end
    post'/teams' do
      @team = Team.new(params[:team])
-     @team.name = params[:team][:name]
-     @team.motto = params[:team][:motto]
+    #  @team.name = params[:team][:name]
+    #  @team.motto = params[:team][:motto]
 
      heros = params[:team][:heros]
      heros.each do |hero|
