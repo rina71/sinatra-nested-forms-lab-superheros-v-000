5f4f5bf1cf1a7ace882778b75f10ce0544631524
Yurina Shin <yurinas@gmail.com>
2018-08-30 17:40:53 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
     erb :team
   end
    post'/teams' do
-     @team = Team.new(params[:team])
+     @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
     #  @team.name = params[:team][:name]
     #  @team.motto = params[:team][:motto]
 
